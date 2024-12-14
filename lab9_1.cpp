43f11ce4f5a7709dc0ae4c75452aa8fab30ff8c2
Chiraphat-t <chiraphat.ttt@gmail.com>
2024-12-14 21:50:53 +0700

    1

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,9 +3,14 @@
 using namespace std;
 
 int main(){	
+	double loan , interest , pay, newbalance , total, fory; 
+	
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interest;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +25,36 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	fory=loan*interest/100;
+	total=loan+fory;
+	newbalance=total-pay;
+
 	
+	for (int i=1; loan!=0; i++)
+	{
+	
+	cout << fixed << setprecision(2); 
+	cout << setw(13) << left << i;
+	
+
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << fory;
+	cout << setw(13) << left << total;
+	if(total<pay){
+		pay=total;
+		newbalance=total-pay;
+	}
+	cout << setw(13) << left << pay;
+	cout << setw(13) << left << newbalance;
+	cout << "\n";
+	loan = newbalance;
+	fory=loan*interest/100;
+	total=loan+fory;
+	newbalance=total-pay;
+	
+	
+	}
+
+
 	return 0;
 }
