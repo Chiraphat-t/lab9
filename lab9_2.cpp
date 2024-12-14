8728c87f827ff2bf34acd878991380a3b4e0f2d3
Chiraphat-t <chiraphat.ttt@gmail.com>
2024-12-14 22:36:14 +0700

    2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,6 +3,26 @@ using namespace std;
 
 //Write the function printO() here
 
+char printO(int a,int b){
+    if (a<=0||b<=0)
+	{
+		cout<<"Invalid input";
+	}
+	
+	for (int i=0; i < a; i++)
+	{
+		for (int j=0; j < b; j++)
+		{
+			cout << "O";
+		}
+		cout << endl;
+		
+	}
+	
+	return 0;
+}
+
+
 int main(){
 	
 	printO(2,2);
